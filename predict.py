ee5c3f0ac65650d83aa92ffdfa74e06ebddc1b15
DaLizardWizard <robson.buckstone@hotmail.co.uk>
2023-12-17 15:27:23 +0000

    cog ready

---
+++ predict.py
@@ -0,0 +1,74 @@
+from cog import BasePredictor, Path, Input
+import torch
+import utils.imgops as ops
+import utils.architecture.architecture as arch
+import cv2
+import numpy as np
+import torch
+import sys
+
+class Predictor(BasePredictor):
+    def setup(self):
+        """Load the model into memory to make running multiple predictions efficient"""
+        NORMAL_MAP_MODEL = 'utils/models/1x_NormalMapGenerator-CX-Lite_200000_G.pth'
+        OTHER_MAP_MODEL = 'utils/models/1x_FrankenMapGenerator-CX-Lite_215000_G.pth'
+
+        def load_model(model_path):
+            self.device = torch.device('cuda')
+            state_dict = torch.load(model_path)
+            model = arch.RRDB_Net(3, 3, 32, 12, gc=32, upscale=1, norm_type=None, act_type='leakyrelu',
+                                    mode='CNA', res_scale=1, upsample_mode='upconv')
+            model.load_state_dict(state_dict, strict=True)
+            del state_dict
+            model.eval()
+            for k, v in model.named_parameters():
+                v.requires_grad = False
+            return model.to(self.device)
+        self.models = [
+            # NORMAL MAP
+            load_model(NORMAL_MAP_MODEL), 
+            # ROUGHNESS/DISPLACEMENT MAPS
+            load_model(OTHER_MAP_MODEL)
+            ]
+
+    def predict(self,
+            image: Path = Input(description="RGB Image to generate maps from"),
+    ) -> Path:
+        """Run a single prediction on the model"""
+        # read image
+        try: 
+            img = cv2.imread(image, cv2.cv2.IMREAD_COLOR)
+        except:
+            img = cv2.imread(image, cv2.IMREAD_COLOR)
+            
+        # Seamless modes
+        img = cv2.copyMakeBorder(img, 16, 16, 16, 16, cv2.BORDER_WRAP)
+        # Make maps
+        def process(self,img, model):
+            img = img * 1. / np.iinfo(img.dtype).max
+            img = img[:, :, [2, 1, 0]]
+            img = torch.from_numpy(np.transpose(img, (2, 0, 1))).float()
+            img_LR = img.unsqueeze(0)
+            img_LR = img_LR.to(self.device)
+
+            output = model(img_LR).data.squeeze(
+                0).float().cpu().clamp_(0, 1).numpy()
+            output = output[[2, 1, 0], :, :]
+            output = np.transpose(output, (1, 2, 0))
+            output = (output * 255.).round()
+            return output
+        
+        rlts = [process(img, model) for model in self.models]
+
+        # ... pre-processing ...
+        normal_map = rlts[0]
+        roughness = rlts[1][:, :, 1]
+        displacement = rlts[1][:, :, 0]
+
+        output_paths = []
+        for i, index in enumerate(normal_map,roughness,displacement):
+            output_path = f"/tmp/out-{i}.png"
+            image.save(output_path)
+            output_paths.append(Path(output_path))
+            
+        return output_paths
